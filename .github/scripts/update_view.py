c58f04da27c7b2ef2fa31ce736c1afd858ddf4d8
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-04-07 11:19:20 +0530

    last update

--- .github/scripts/update_view.py
+++ .github/scripts/update_view.py
@@ -146,5 +146,6 @@ def update_view(issue_message, token) -> None:
 if __name__ == "__main__":
     print(sys.argv)
     issue_message = sys.argv[1]
-    token = sys.argv[2]
+    token = os.environ.get("GITHUB_TOKEN")
+    print(token)
     update_view(issue_message, token)
