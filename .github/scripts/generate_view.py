b2b5af145ee85a586890c98fc6bfa88dc06c7008
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-04-07 12:42:04 +0530

    last update

--- .github/scripts/generate_view.py
+++ .github/scripts/generate_view.py
@@ -1,156 +1,109 @@
-import logging
-import os
-import sys
-from enum import Enum
+import re
 from pathlib import Path
+from typing import Dict
 
-from collection.views.hfml import HFMLView
+from collection.items.alignment import Alignment
+from collection.items.collection_meta import CollectionMeta
+from collection.items.pecha import Pecha, PechaFragment
+from collection.items.work import Work
+from collection.utils import get_item
 from collection.views.plain_text import PlainTextView
-from generate_view import generate_view
-from github import Github
+from collection.items.item import Item
+from collection.views.view import View
+from openpecha.config import BASE_PATH
 from openpecha.utils import load_yaml
 
-OWNER = "jungtop"
-
-logging.basicConfig(
-    filename="basefile_metadata.log",
-    format="%(levelname)s: %(message)s",
-    level=logging.INFO,
-)
-
-class ViewEnum(Enum):
-    PlainTextView = PlainTextView
-    HFMLView = HFMLView
+OPENPECHA_DATA_PREFIX_URL = "https://github.com/OpenPecha-Data"
 
 
-def notifier(msg):
-    logging.info(msg)
+class ItemEnum:
+    pecha = Pecha
+    alignment = Alignment
+    work = Work
+    pecha_fragment = PechaFragment
 
 
-def extract_pecha_ids(msg):
-    pecha_ids = [i.strip() for i in msg.split(",")]
-    return pecha_ids
-
-
-def update_repo(g, repo_name, file_path, commit_msg, new_content):
+def get_op_item_meta(item_id:str, item_path:str):
     """
-    This functions updated the repo to github
-    :param g:Github Obj
-    :param repo_name: name of the repo 
-    :param file_path: Path of the file to be updated
-    :param commit_msg: Commit message
-    :param new_cotent: new content to be push in the file_path
-    :return: None
+    This function gives the meta of a item
+    :param item_id:id of item
+    :param item_path:path of the item
+    ;return:meta in str
     """
-
-    repo = g.get_repo(f"{OWNER}/{repo_name}")
-    contents = repo.get_contents(f"{file_path}", ref="main")
-    repo.update_file(
-        path=contents.path,
-        message=commit_msg,
-        content=new_content,
-        sha=contents.sha,
-        branch="main",
-    )
-    try:
-        repo = g.get_repo(f"{OWNER}/{repo_name}")
-        contents = repo.get_contents(f"{file_path}", ref="main")
-        repo.update_file(
-            path=contents.path,
-            message=commit_msg,
-            content=new_content,
-            sha=contents.sha,
-            branch="main",
-        )
-        print(f"{repo_name} updated ")
-    except Exception as e:
-        print(f"{repo_name} not updated with error {e}")
+    meta = None
+    if re.match("^I", item_id):
+        meta_path = Path(f"{item_path}/{item_id}.opf/meta.yml")
+        meta = load_yaml(meta_path)
+    return meta
 
 
-def push_view(file_path:str,commit_msg:str,new_content:str,token:str) -> None:
+def get_item_attr(dic:Dict, item_path:str,item_cls:Item):
     """
-    This function pushes the new view created to the repository itself
-    :param file_path: path of the file in the repo
-    :param commit_msg: commit message
-    :new_content: new content to write in the file_path
-    :token: Gtihub Token
-    :return: None
+    This function gives the attributes of the item_cls
+    :param dic: The raw dic containg meta of the item
+    :param item_path: Path of the item in str
+    :pararm item_cls: Type of Item class
+    :return:A dic contating all the attributes of the item_cls 
     """
-    g = Github(token)
-    collection_id = os.getenv("REPO_NAME")
-    update_repo(g, collection_id, file_path, commit_msg, new_content)
+    pecha = {}
+    pecha_attrs = item_cls.__annotations__.keys()
+    for pecha_attr in pecha_attrs:
+        if pecha_attr in dic.keys():
+            pecha[pecha_attr] = dic[pecha_attr]
+        else:
+            pecha[pecha_attr] = None
+    pecha["path"] = item_path
+    return pecha
 
 
-def push_views(views_path, view_type, token):
+def get_item_cls(item_id:str):
     """
-    This function pushes the new views created to the repository itself
-    :param views_path: Path of Views
-    :view_type: Type of View
-    :token: Gtihub Token
-    :return: None
+    This function return item class base on the ID
+    :param item_id: id of the item
+    :return:item class
     """
-    for view_path in views_path:   
-        collection_id = os.getenv("REPO_NAME")
-        base_id = view_path.stem
-        view_name = f"{base_id}.txt"
-        file_path = f"{collection_id}.opc/views/{view_type}/{view_name}"
-        view = view_path.read_text(encoding="utf-8")
-        commit_msg = f"Updated {view_path.name}"
-        push_view(file_path,commit_msg,view,token)
+
+    if item_id.startswith("A"):
+        item_class = ItemEnum.alignment
+    elif item_id.startswith("W"):
+        item_class = ItemEnum.work
+    elif item_id.startswith("I"):
+        item_class = ItemEnum.pecha
+    return item_class
 
 
-def get_view_types(item_id):
+def get_collection_meta(collection_id:str):
     """
-    This fucntion returns view types in a collection of a item id
-    :param item_id: item id
-    :return views: Returns list of view 
+    This function return the meta of the collection considering 
+    it's in action pwd.
+    :param collection_id:Collection id
+    :return: meta in str
     """
-    views = []
-    meta_path = Path(f"./meta.yml")
+    meta_path = Path(f"{collection_id}.opc/meta.yml")
     meta = load_yaml(meta_path)
-    view_types = meta["item_views_map"]
-    for view_type, body in view_types.items():
-        if item_id in list(body.keys()):
-            views.append(view_type)
-
-    return views
+    return meta
 
 
-def get_view_class(view_name: str):
+def generate_view(op_item_id: str, view: View, output_dir: Path = None):
     """
-    This function return the view class associated with view name
-    :param view_name: Name of the view in str
-    :return:View Class
+    This function generated views of a item
+    :param op_item_id: item id
+    :param view: View Object
+    :param output_dir: output path of the view
+    :return: list of path of generated views in Path.
     """
-    try:
-        for e in ViewEnum:
-            if e.name == view_name:
-                return e.value
-    except ValueError:
-        print(f"Unknown View Class {view_name}")
-        return []
-
-
-def update_view(issue_message, token) -> None:
-    """
-    This function updates the view when given a issue message of items with
-    tokens.
-    :params issuse_message: a stirng which consist op item ids
-    :token : github token to push the changes
-    :return : None
-    """
-    pecha_ids = extract_pecha_ids(issue_message)
-    for pecha_id in pecha_ids:
-        view_types = get_view_types(pecha_id)
-        for view_type in view_types:
-            view = get_view_class(view_type)
-            views_path = generate_view(pecha_id, view())
-            if views_path:
-                push_views(views_path, view_type, token)
+    Path("./data").mkdir(parents=True, exist_ok=True)
+    if output_dir is None:
+        output_dir = BASE_PATH
+    op_item_path = get_item(op_item_id)
+    meta = get_op_item_meta(op_item_id, op_item_path)
+    item_cls = get_item_cls(op_item_id)
+    item_attr = get_item_attr(meta, op_item_path,item_cls)
+    item_obj = item_cls(**item_attr)
+    serializer = view.serializer
+    views_path = serializer().serialize(item=item_obj, output_dir=Path("./data"))
+    return views_path
 
 
 if __name__ == "__main__":
-    print(sys.argv)
-    issue_message = sys.argv[1]
-    token = sys.argv[2]
-    update_view(issue_message, token)
+    generate_view("I3D4F1804", PlainTextView(), Path("./data"))
